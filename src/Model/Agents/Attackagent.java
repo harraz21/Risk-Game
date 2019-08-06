390a1da2649c9ddce163284e72117faed5610fac
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-08-06 09:12:53 +0200

    Start Attacking Agent O:)

--- src/Model/Agents/Attackagent.java
+++ src/Model/Agents/Attackagent.java
@@ -2,7 +2,7 @@ package Model.Agents;
 
 public class Attackagent extends Agent {
 
-    ArrayList<Territory> myTerritory;
+    //ArrayList<Territory> myTerritory; ->Agent Class
     ArrayList<String> neighbours;
     public String playerName;
 
