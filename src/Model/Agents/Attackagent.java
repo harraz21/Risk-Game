006a9922d1239365ad6f79748f5fb798b2e550ba
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-08-06 10:49:17 +0200

    continuing Attacking Agent :'(

--- src/Model/Agents/Attackagent.java
+++ src/Model/Agents/Attackagent.java
@@ -1,10 +1,25 @@
 package Model.Agents;
+import java.util.HashMap;
+import java.util.Random;
 
 public class Attackagent extends Agent {
 
-    //ArrayList<Territory> myTerritory; ->Agent Class
+    //ArrayList<Territory> myTerritory;->Agent Class
     ArrayList<String> neighbours;
     public String playerName;
+    static boolean attackAgain ;
+    String attackSameCountryAgain ;
+
+
+    public static void attackCountry(String playerName, HashMap<Terrritory> list_T) {
+        attackAgain = true;
+        attackSameCountryAgain= "Yes";
+        boolean adjacencyCheck = false;
+        boolean noOfArmiesCheck = false;
+        boolean checkValidityOfDef = false;
+
+
+    }
 
 
 
