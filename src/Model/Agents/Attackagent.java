a221dc37c53377f6c2d254387cef4d2dfd8a3b5d
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-08-06 09:06:51 +0200

    Atart Attacking Agent O:)

---
+++ src/Model/Agents/Attackagent.java
@@ -0,0 +1,11 @@
+package Model.Agents;
+
+public class Attackagent extends Agent {
+
+    ArrayList<Territory> myTerritory;
+    ArrayList<String> neighbours;
+    public String playerName;
+
+
+
+}
