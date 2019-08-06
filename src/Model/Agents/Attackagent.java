c8e2cc4b3f76133c83b03c241d13e012f47ab0d6
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-08-06 20:30:01 +0200

    شلنا الي ملوش لازمة

--- src/Model/Agents/Attackagent.java
+++ src/Model/Agents/Attackagent.java
@@ -3,16 +3,12 @@ import java.util.HashMap;
 import java.util.Random;
 
 import Model.Map.Territory;
-
 import java.util.ArrayList;
 
 public class Attackagent extends Agent {
 
-    //ArrayList<Territory> myTerritory;->Agent Class
-    ArrayList<String> neighbours;
-    public String playerName;
-    static boolean attackAgain ;
-    String attackSameCountryAgain ;
+
+
 
     Public Territory getAttacker() {
         if(this.canAttack())
