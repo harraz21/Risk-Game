6b73199ffa7a88e5d1be60c7207ae0622945ef6f
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-08-07 17:41:45 +0200

    fixing srekh xD

--- src/Model/Agents/Attackagent.java
+++ src/Model/Agents/Attackagent.java
@@ -3,8 +3,11 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
 
+import Model.Agents.ArmyUnits.ArmyUnits;
 import Model.Map.Territory;
 import java.util.ArrayList;
+
+import static sun.management.MemoryUsageCompositeData.getMax;
 
 public class Attackagent extends Agent {
      Territory attacker;
@@ -12,7 +15,7 @@ public class Attackagent extends Agent {
 
 
     public Territory getAttacker() {
-        if(this.continueAttacking())
+        if(this.continueAttacking()==0)
         {
 
 
@@ -52,15 +55,19 @@ public class Attackagent extends Agent {
         return min;
     }
 
-    public boolean continueAttacking()
+    public int  continueAttacking()
     {
-        return (this.attacker== getMax() && this.attack());
+        ArmyUnits xx=new ArmyUnits();
+        Territory b = new Territory();
+         int v= xx.getNoOfUnits();
+         int b = b.getNeighboringTerritories();
+        int c =getMax(v,b);
+        return c ;
     }
-    protected void setNOofAttackingArmies() {
-        attacker.armyUnits = attacker.armyUnits - 1;
-        if(attacker.armyUnits > 3)
-            attacker.armyUnits = 3;
-    }
+
+
+
+
 }
 
 
