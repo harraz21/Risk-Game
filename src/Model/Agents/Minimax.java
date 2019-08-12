5e38dc1e2409b4b3a32b6e48a03c97dacfd57e02
harraz21 <mohamedharraz21@gmail.com>
2019-08-12 22:05:45 +0200

    pseudo code minMax

--- src/Model/Agents/Minimax.java
+++ src/Model/Agents/Minimax.java
@@ -6,30 +6,39 @@ import javafx.util.Pair;
 import utility.Utility;
 
 public class Minimax {
-    public Pair<GameState,Integer> minMaxDecision(GameState position , int depth, int beta, boolean maximizingAgent)
-    {
-        if (depth == 0 /**  || game is over**/)
-        {
-            Pair<GameState,Integer> pair = new Pair<>(position,Utility.eval(position));
+    public Pair<GameState, Integer> minMaxDecision(GameState position, int depth, int alpha, int beta, boolean maximizingAgent) {
+        if (depth == 0 /**  || game is over**/) {
+            Pair<GameState, Integer> pair = new Pair<>(position, Utility.eval(position));
 
             return pair;
         }
 
-        if (maximizingAgent)
-        {
+        if (maximizingAgent) {
             int maxEval = Integer.MIN_VALUE;
             /**for (: child for position  ) {
-                int eval = minMaxDecision(child,depth-1,false);
+             int eval = minMaxDecision(child,depth-1,false);
              maxEval = max(maxEval,eval)
-            }
+             alpha = max(alpha,eval)
+             if (beta <= alpha)
+             {
+             break;
+             }
+             }
              return the value and the last gamestate returned that value
              **/
-        }else {
+        } else {
             int minEval = Integer.MAX_VALUE;
             /**
              * foreach child
+             * {
              * eval = minimax(child ,depth-1,true)
              * minEval = min(minEval,eval)
+             * beta = min(beta,eval)
+             * if (beta <= alpha)
+             *    {
+             *           break;
+             *    }
+             * }
              *
              * return the value and the last gamestate returned that value
              * **/
@@ -37,7 +46,6 @@ public class Minimax {
         //temporary
         return null;
     }
-
 
 
 }
