a09ef3f8f3adeebb7cab61540d3e1818dfa3d43c
harraz21 <mohamedharraz21@gmail.com>
2019-08-12 02:03:20 +0200

    added list of agents.

--- src/sample/StartingSceneController.java
+++ src/sample/StartingSceneController.java
@@ -1,10 +1,15 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 import java.net.URL;
@@ -13,9 +18,28 @@ import java.util.ResourceBundle;
 import static sample.Main.primarystage;
 
 public class StartingSceneController implements Initializable {
+    @FXML
+    public ComboBox firstAgentComboBox;
+    @FXML
+    public ComboBox secondAgentComboBox;
+    @FXML
+    public ComboBox mapComboBox;
+    @FXML
+    public Label AlertLabel;
+    ObservableList<String> firstAgentList = FXCollections.observableArrayList("Human Agent","Passive Agent","Aggressive Agent"
+            ,"Pacifist Agent","Greedy Agent","A* Agent","Real time A* Agent","Minimax Agent");
+    ObservableList<String> secondAgentList = FXCollections.observableArrayList("Human Agent","Passive Agent","Aggressive Agent"
+            ,"Pacifist Agent","Greedy Agent","A* Agent","Real time A* Agent","Minimax Agent");
+    ObservableList<String> mapList = FXCollections.observableArrayList("Egypt","USA");
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        firstAgentComboBox.setValue("Human Agent");
+        firstAgentComboBox.setItems(firstAgentList);
+        secondAgentComboBox.setValue("Greedy Agent");
+        secondAgentComboBox.setItems(secondAgentList);
+        mapComboBox.setValue("Egypt");
+        mapComboBox.setItems(mapList);
     }
 
     public void startButtonOnAction(ActionEvent actionEvent) throws IOException {
